c189ff476deadc5283459123670e12880879801e
jitumbile <jitumobilemail@gmail.com>
2026-08-05 18:13:49 +0530

    make cookies file writable so yt-dlp save_cleanup succeeds

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ -n "$YT_COOKIES_B64" ]; then
   mkdir -p /etc/ytdl
   echo "$YT_COOKIES_B64" | base64 -d > /etc/ytdl/cookies.txt
-  chmod 600 /etc/ytdl/cookies.txt
+  chmod 666 /etc/ytdl/cookies.txt
 fi
 
 exec "$@"
